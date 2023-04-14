ea9032368b29a219fff7ac31935c1a83fb740b80
katyacher <katyacher@inbox.ru>
2023-04-14 09:41:55 +0300

    Initial commit

--- build/CMakeFiles/27_2_shapes.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for 27_2_shapes.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kate/Documents/skillbox/27_2_shapes
-# Build directory: /home/kate/Documents/skillbox/27_2_shapes/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
